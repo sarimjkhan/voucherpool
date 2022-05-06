78a8cfc0b71ed36eb2939a4202a7d12f71249e73
Sarim Javaid Khan <sarim.javaid@backcountry.com>
2022-05-06 18:25:34 +0400

    default json rendered added

--- voucherpool/voucherpool/voucherpool/settings.py
+++ voucherpool/voucherpool/voucherpool/settings.py
@@ -125,6 +125,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
