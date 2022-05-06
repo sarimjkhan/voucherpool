1300da68fee6a811570aca85308389fd8acc8771
Sarim Javaid Khan <sarim.javaid@backcountry.com>
2022-05-06 17:09:58 +0400

    Debug mode turned off

--- voucherpool/voucherpool/voucherpool/settings.py
+++ voucherpool/voucherpool/voucherpool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8kej@g^04pbk%mryd!2!e6&xchkb^whk5p)-)_buy*49%4yc_5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
